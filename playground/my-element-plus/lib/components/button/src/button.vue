6ff954643e80297fbda21987037f2ae6cc0e73b3
Laishuxin <15622971239@163.com>
2022-11-07 21:31:29 +0800

    feat(hook): add use-namespace utils

--- playground/my-element-plus/lib/components/button/src/button.vue
+++ playground/my-element-plus/lib/components/button/src/button.vue
@@ -5,6 +5,12 @@
 </template>
 
 <script setup lang="ts">
+import { useNamespace } from '../../../hooks/use-namespace';
+
+defineOptions({ name: 'ElButton' });
+
+const ns = useNamespace('button');
+
 defineProps();
 </script>
 
